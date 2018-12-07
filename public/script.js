b031f34350a21e2c89b21a9a7e3f3df2fbb03b9d
danielstena <32764564+danielstena@users.noreply.github.com>
2018-12-07 11:48:49 +0100

    Stor progress
    Grundstruktur klar och 1:a klar

--- public/script.js
+++ public/script.js
@@ -1,39 +1,47 @@
 $(document).ready(function(){
-  
-update();  
+    var d = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var hours = addZeroBefore(h);
+    var minutes = addZeroBefore(m);
+    var total_tid = hours + ":" + minutes
 
-function update(){
+    function addZeroBefore(n) {
+        return (n < 10 ? '0' : '') + n;
+    }
+    
+
+update(total_tid);  
+
+function update(total_tid){
     $.ajax({
         type: "GET",
-        url: '/test',
+        url: '/resor',
         dataType: 'json',
         data: {"data":"check"},
         success: function(data){
-            $(".container").html("");
-            $(".container").append("<div class='row'><div class='cell_buss'>"+ data[0].name+"</div><div class='cell'>"+ data[0].time+"</div><div class='cell'>"+ data[0].direction+"</div></div>");
 
-            $(".container").append("<div class='row'><div class='cell_buss'>"+ data[1].name+"</div><div class='cell'>"+ data[1].time+"</div><div class='cell'>"+ data[1].direction+"</div></div>");
-
-            $(".container").append("<div class='row'><div class='cell_buss'>"+ data[2].name+"</div><div class='cell'>"+ data[2].time+"</div><div class='cell'>"+ data[2].direction+"</div></div>");
-
-            $(".container").append("<div class='row'><div class='cell_buss'>"+ data[3].name+"</div><div class='cell'>"+ data[3].time+"</div><div class='cell'>"+ data[3].direction+"</div></div>");
+            $("#time").html("<h1>"+ total_tid + "</h1>");
             
-            // $.each(data, function () {
-                //     $.each(this, function (name,value) {
-                    //         console.log(typeof value)
-                    //         // $(".row").append("<div class='cell'>"+ value.name+"</div><div class='cell'>"+ value.time+"</div>");
-                    //     })
-                    // });
+            $("#1").append("<div class='cell stop'>" + data.stop.slice(0,15) + " </div><div class='cell bus'>" + data.name.slice(5,7) + "</div><div class='cell' id='first'>" + data.first_time + " min</div><div class='cell' id=''second>" + data.second_time + " min</div><div class='cell' id='direction'>"+ data.direction+"</div><div class='cell' id='track'>"+ data.track+"</div>");
+
+            // $(".container").append("<div class='row'><div class='cell_buss'>" + data[0].mot_tuve[0].name + "</div><div class='cell'>" + data[0].mot_ostra[0].time + "</div><div class='cell'>"+ data[0].tuve[0].direction+"</div></div>");
+
+
+            // $(".container").append("<div class='row'><div class='cell_buss'>" + mall(2,"name") + "</div><div class='cell'>" + mall(2,"time") + "</div><div class='cell'>" + mall(2,"direction") + "</div></div>");
+
+            // $(".container").append("<div class='row'><div class='cell_buss'>" + mall(3,"name") + "</div><div class='cell'>" + mall(3,"time") + "</div><div class='cell'>" + mall(3,"direction") + "</div></div>");
+            
                     
             }
         });
     }
     setInterval(function()
     { 
-        update();  
-        console.log("+1") 
+        update(total_tid);  
+        location.reload();
     }
-    , 3000);
+    , 30000);
 
 });
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
     const express = require('express');
     var Base64 = require('Base-64');
     const superagent = require('superagent');
-
+    const moment = require('moment')
     var request = require('request');
     var pug = require('pug');
     var fs = require('fs');
@@ -13,10 +13,10 @@
 
 // -- STARTAR SERVER --  //
 
-    const app = express()
-    const port = 3000
+const app = express()
+const port = 3000
 
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // -- STARTAR SERVER -- //
 
@@ -26,75 +26,113 @@ app.use(express.static("public"))
 
     // -- För PUG -- //
 
-        app.set('view engine', 'pug');
+    app.set('view engine', 'pug');
 
-        app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({extended: true}));
 
-        // -- För PUG -- //
-        
-        //------------------------------------------------------------------ //
-        
-        // -- VARIABLES -- //
-        
-        const key = '27ZODCx9Dwf71auxcafiA2va3bAa';
-        const secret = 'RzHR_Nr9Q6CndGJs9eCSsfgqIVIa';
-        var deviceId = "unit_one";
-        var callback;
-        var smorslott_mot_ostra;
-        var json_sm_mot_ostra = [];
-        var json_sm_mot_tuve = [];
-        var id;
-    //Encryptation of key and secret
-    var code = Base64.encode(key + ":" + secret);
+    // -- För PUG -- //
 
-// -- VARIABLES -- //
+//------------------------------------------------------------------ //
+
+    // -- VARIABLES -- //
+            
+            const key = '27ZODCx9Dwf71auxcafiA2va3bAa';
+            const secret = 'RzHR_Nr9Q6CndGJs9eCSsfgqIVIa';
+            var deviceId = "unit_one";
+            var callback;
+            var smorslott_mot_ostra;
+            var json_sm_mot_ostra = [];
+            var alla_resor = [];
+            var json_sm_mot_tuve = [];
+            var id;
+
+            //Encryptation of key and secret
+            var code = Base64.encode(key + ":" + secret);
+
+    // -- VARIABLES -- //
+
+//------------------------------------------------------------------ //
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
 
 use_token = async function(token){
     var d = new Date();
     var date = d.toLocaleDateString();
     var hours = d.getHours();
     var minutes = d.getMinutes();
-    const access_token = token;
     
-    // console.log(hours + ":" + minutes)
-    const url_sm_tuve = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=9022014006100001&date=" + date + "&time=" + hours + ":" + minutes + "%3A06&direction=9022014007100001&format=json";
-    const url_sm_ostra = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=9022014006100001&date=" + date + "&time=" + hours + ":" + minutes + "%3A06&direction=9022014007880005&format=json";
+    const url_smorslott_17_tuve = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=9022014006100001&date=" + date + "&time=" + hours + ":" + minutes + "%3A06&direction=9022014007100001&format=json";
+    const url_smorslott_17_ostra = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=9022014006100001&date=" + date + "&time=" + hours + ":" + minutes + "%3A06&direction=9022014007880005&format=json";
+    const url_backv_svart_amhult = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=9022014001245003&date=" + date + "&time=" + hours + ":" + minutes + "%3A06&direction=9022014001004007&format=json";
+   // const url_backv_svart_vallhamra = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=9022014006100001&date=" + date + "&time=" + hours + ":" + minutes + "%3A06&direction=9022014007880005&format=json";
+   // const url_ostra_1an = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=9022014006100001&date=" + date + "&time=" + hours + ":" + minutes + "%3A06&direction=9022014007880005&format=json";
     
-
     const headers = {
         // 'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Bearer ${access_token}`
+        'Authorization': `Bearer ${token}`
     }
 
     // -- SMÖRSLOTTSGATAN/MOT TUVE -- //
 
-        await request(url_sm_tuve, { headers },(err, res, body) => {
+        await request(url_smorslott_17_tuve, { headers },(err, res, body) => {
             if (err) { return console.log(err); }
 
             smorslott_mot_tuve = JSON.parse(body);
 
-            for (var i = 0; i < 20; i++) { 
-                json_sm_mot_tuve.push({time: smorslott_mot_tuve.DepartureBoard.Departure[i].time, direction: smorslott_mot_tuve.DepartureBoard.Departure[i].direction});
-            }
+            app.get('/test', (req, res) => res.send(smorslott_mot_tuve))
+
+            var sliced_hours_first = smorslott_mot_tuve.DepartureBoard.Departure[0].time.slice(0, 2);
+            var sliced_minutes_first =  smorslott_mot_tuve.DepartureBoard.Departure[0].time.slice(3, 5);
+            var api_date_first = smorslott_mot_tuve.DepartureBoard.Departure[0].date;
+
+            var sliced_hours_second = smorslott_mot_tuve.DepartureBoard.Departure[1].time.slice(0, 2);
+            var sliced_minutes_second =  smorslott_mot_tuve.DepartureBoard.Departure[1].time.slice(3, 5);
+            var api_date_second = smorslott_mot_tuve.DepartureBoard.Departure[1].date;
+            // TEST //
+
+                current_date = new Date()
+
+                first_buss = new Date(api_date_first +"T"+ sliced_hours_first +":" + sliced_minutes_first + ":00");
+                second_buss = new Date(api_date_second +"T"+ sliced_hours_second +":" + sliced_minutes_second + ":00");
+
+                function diff_minutes_first(current_date, first_buss) 
+                {
+                    var first_diff =(first_buss.getTime() - current_date.getTime()) / 1000;
+                    first_diff /= 60;
+                
+                    return Math.abs(Math.round(first_diff));
+                }
+                // diff_minutes_first(current_date, first_buss);
+                
+                function diff_minutes_second(current_date, second_buss) 
+                {
+                    var second_diff =(second_buss.getTime() - current_date.getTime()) / 1000;
+                    second_diff /= 60;
+                    return Math.abs(Math.round(second_diff));
+                }
+                // diff_minutes_second(current_date, second_buss);
+
+                json_sm_mot_tuve = ({track: smorslott_mot_tuve.DepartureBoard.Departure[0].track, stop: smorslott_mot_tuve.DepartureBoard.Departure[0].stop, name: smorslott_mot_tuve.DepartureBoard.Departure[0].name, first_time: diff_minutes_first(current_date, first_buss), second_time: diff_minutes_second(current_date, second_buss), direction: smorslott_mot_tuve.DepartureBoard.Departure[0].direction});
         }); 
     
     // -- SMÖRSLOTTSGATAN/MOT ÖSTRA SJUKHUSET -- //
     
-        await request(url_sm_ostra, { headers },(err, res, body) => {
+        await request(url_smorslott_17_ostra, { headers },(err, res, body) => {
             if (err) { return console.log(err); }
             smorslott_mot_ostra = JSON.parse(body);
-            // console.log(smorslott.DepartureBoard.Departure[0])
             
             for (var i = 0; i < 20; i++) { 
                 json_sm_mot_ostra.push({name: smorslott_mot_ostra.DepartureBoard.Departure[0].name, time: smorslott_mot_ostra.DepartureBoard.Departure[i].time, direction: smorslott_mot_ostra.DepartureBoard.Departure[i].direction});
+                alla_resor.push({mot_ostra: json_sm_mot_ostra, mot_tuve: json_sm_mot_tuve});
+
             }         
-        });  app.get('/test', (req, res) => res.send(json_sm_mot_ostra))
-            app.get('/', (req, res) => res.render('index',{json_sm_mot_tuve, json_sm_mot_ostra}))
+        });  
+        app.get('/resor', (req, res) => res.send(json_sm_mot_tuve))
+        // console.log(alla_resor[0].mot_ostra[0])
+        // app.get('/resor', (req, res) => res.send(alla_resor))
+        
+        app.get('/', (req, res) => res.render('index'))
 }   
 
 function get_token(code, deviceId, callback) {
